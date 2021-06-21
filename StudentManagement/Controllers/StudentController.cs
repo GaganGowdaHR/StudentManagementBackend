24eef1d0367a7c45c76291f8d15fdb652976453c
Gagan Gowda HR <erp.gagan.loginware@gmail.com>
2021-06-21 14:27:47 +0530

    Add project files.

---
+++ StudentManagement/Controllers/StudentController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"
+                            select StudentID, StudentName,Course
+                            from dbo.Students
+                            ";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Models.Student student)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @" insert into dbo.Students (StudentName,Course) Values ('" + student.StudentName + @"','" + student.Course + @"')";
+
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Add Successfully";
+
+            }
+            catch (Exception)
+            {
+                return "Failed to Add";
+            }
+        }
+
+        public string Put(Models.Student dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                            update dbo.Students set StudentName= '" + dep.StudentName + @"',Course = '" + dep.Course +@"'
+                                where StudentID = " + dep.StudentID + @"";
+
+
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Updated Successfully";
+
+            }
+            catch (Exception)
+            {
+                return "Failed to Update";
+            }
+        }
+        public string Delete(int id)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                            delete from dbo.Students where StudentID = " + id;
+
+
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentAppDB"].ConnectionString))
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Deleted Successfully";
+
+            }
+            catch (Exception)
+            {
+                return "Failed to Delete";
+            }
+        }
+    }
+}
---
+++ StudentManagement/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentManagement.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public HttpResponseMessage Get()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("StudentID");
+            dt.Columns.Add("StudentName");
+            dt.Columns.Add("Course");
+
+            dt.Rows.Add("1", "Sam", "PCB");
+            dt.Rows.Add("2", "John", "PCM");
+
+            return Request.CreateResponse(HttpStatusCode.OK, dt);
+        }
+
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
